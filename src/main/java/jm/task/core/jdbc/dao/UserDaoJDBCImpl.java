590ba556d455671d0ad753931db3e319619d81e0
Daniil Rubak <arhdark@yandex.ru>
2023-04-19 00:19:13 +0300

    DAO and Service refactoring

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -4,7 +4,6 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
-import java.sql.SQLSyntaxErrorException;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -19,7 +18,7 @@ public class UserDaoJDBCImpl implements UserDao {
                     "name VARCHAR(128)," +
                     "lastName VARCHAR(128)," +
                     "age TINYINT)");
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
@@ -34,17 +33,17 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String text = String.format("INSERT INTO user(name, lastName, age) VALUES ('%s','%s',%s)",name,lastName,age);
+        String text = String.format("INSERT INTO user(name, lastName, age) VALUES ('%s','%s',%s)", name, lastName, age);
         try {
             Util.doSomething(text);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("User с именем – " + name  + " добавлен в базу данных");
+        System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
-        String text = String.format("DELETE FROM user WHERE id = %s",id);
+        String text = String.format("DELETE FROM user WHERE id = %s", id);
         try {
             Util.doSomething(text);
         } catch (SQLException e) {
@@ -54,7 +53,16 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         try {
-            return Util.getUserList();
+            return Util.getList("SELECT name,lastName,age FROM User", (rs) -> {
+                try {
+                    String userName = rs.getString("name");
+                    String userLastName = rs.getString("lastName");
+                    Byte userAge = rs.getByte("age");
+                    return new User(userName, userLastName, userAge);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,40 +1,37 @@
 package jm.task.core.jdbc.util;
 
-import jm.task.core.jdbc.model.User;
-
 import java.sql.*;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class Util {
-    private static final String URL ="jdbc:mysql://localhost:3306/mydbtest";
-    private static final  String USERNAME = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
-    private Util () {
+
+    private Util() {
     }
 
-    public static void doSomething (String sqlReqvest) throws SQLException {
+    public static void doSomething(String sqlReqvest) throws SQLException {
         DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-        try (Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
              Statement statement = connection.createStatement()) {
             statement.execute(sqlReqvest);
         }
     }
-    public static List <User> getUserList () throws SQLException {
-        List list = new LinkedList();
+
+    public static <T> List<T> getList(String sql, Function<ResultSet, T> function) throws SQLException {
         DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-        try (Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-             Statement statement = connection.createStatement()) {
-            ResultSet rs = statement.executeQuery("SELECT name,lastName,age FROM User");
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+            List<T> list = new ArrayList<>();
             while (rs.next()) {
-//                int id = rs.getInt("id");
-                String userName = rs.getString("name");
-                String userLastName = rs.getString("lastName");
-                Byte userAge = rs.getByte("age");
-                User user = new User(userName,userLastName,userAge);
-                list.add(user);
+                list.add(function.apply(rs));
             }
+            return list;
         }
-        return list;
+
     }
 }
