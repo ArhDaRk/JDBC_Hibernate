66b61a70cd53e14ffa0d39694265dfa600910271
Daniil Rubak <arhdark@yandex.ru>
2023-04-21 12:59:36 +0300

    add main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,18 @@
 package jm.task.core.jdbc;
-
-
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Ivan", "Ivanov", (byte) 20);
+        userDaoJDBC.saveUser("Petr", "Petrov", (byte) 21);
+        userDaoJDBC.saveUser("Andrey", "Andreev", (byte) 22);
+        userDaoJDBC.saveUser("Vasya", "Vasylev", (byte) 23);
+        userDaoJDBC.getAllUsers();
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
+        Util.closeConnection();
     }
 }
